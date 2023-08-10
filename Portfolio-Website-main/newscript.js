67720eff6e1a0b4440e6a323a0a5267671a240d9
Your Name <you@example.com>
2023-08-10 23:40:33 +0530

    second commit

---
+++ Portfolio-Website-main/newscript.js
@@ -0,0 +1,11 @@
+let btn=document.querySelector("#btn");
+let sidebar=document.querySelector(".sidebar");
+btn.addEventListener("click",()=>{
+    document.querySelector('.sidebar').classList.toggle('active');
+    document.querySelector('.home-content').classList.toggle('super');
+     
+})
+
+
+
+
